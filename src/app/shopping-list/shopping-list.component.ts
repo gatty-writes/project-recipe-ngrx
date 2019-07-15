0164c7b62def8ac2a66d665cfdbd30b349d1ef41
Gatty Writes <52652314+gatty-writes@users.noreply.github.com>
2019-07-15 15:55:29 +0530

    new and edit recipe templates
    new and edit recipe templates

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingrediant } from '../shared/ingrediant.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingrediants: Ingrediant[];
+  private igChangeSub: Subscription;
   constructor(private slService: ShoppingListService) {
   }
 
   ngOnInit() {
     this.ingrediants = this.slService.getIngrediants();
-    this.slService.ingrediantAdded.subscribe((ingrediants: Ingrediant[]) => {
+    this.igChangeSub = this.slService.ingrediantAdded.subscribe((ingrediants: Ingrediant[]) => {
       this.ingrediants = ingrediants;
     })
+  }
+
+  ngOnDestroy() {
+    this.igChangeSub.unsubscribe();
   }
 
 }
