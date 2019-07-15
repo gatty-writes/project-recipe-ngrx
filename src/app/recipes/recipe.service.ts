6210e1a769c55f33979049fb36117dbc47e849ee
Gatty Writes <52652314+gatty-writes@users.noreply.github.com>
2019-07-15 13:31:27 +0530

    child routes for the recipe details and item
    child routes for the recipe details and item

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -36,4 +36,8 @@ export class RecipeService {
     addIngrediantsToShoppingList(ingrediants: Ingrediant[]) {
         this.slService.addIngrediants(ingrediants)
     }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
 }
