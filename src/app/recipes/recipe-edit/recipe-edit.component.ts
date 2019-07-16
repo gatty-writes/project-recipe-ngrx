9b2832f7d19cb06ff292dfc14ba1d71f7de46c4c
Gatty Writes <52652314+gatty-writes@users.noreply.github.com>
2019-07-16 15:25:14 +0530

    recipe new and edit responsive page
    recipe new and edit responsive page

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +12,39 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode: boolean = false;
-  constructor(private route: ActivatedRoute) { }
+  editedRecipeItem: Recipe;
+  recipeForm: FormGroup;
+  constructor(private route: ActivatedRoute,
+    private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       });
+  }
+
+  initForm() {
+    let recipeName = '';
+    let  recipeDescription = '';
+    let  recipeImagePath = '';
+    if(this.editMode) {
+      this.editedRecipeItem = this.recipeService.getRecipe(this.id);
+      recipeName = this.editedRecipeItem.name;
+      recipeDescription = this.editedRecipeItem.description;
+      recipeImagePath = this.editedRecipeItem.imagePath;
+    }
+    this.recipeForm = new FormGroup({
+      recipeName: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription)
+    })
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm.value);
   }
 
 }
