75619ccb37b9734d2c4e5533815c88f8e24cb861
Gatty Writes <52652314+gatty-writes@users.noreply.github.com>
2019-07-23 16:18:56 +0530

    state management of the page on login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dsService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean = false;
+  subs = new Subscription();
+  constructor(private dsService: DataStorageService,
+    private authService: AuthService) { }
 
   ngOnInit() {
+    this.subs = this.authService.user$.subscribe(user => {
+      this.isAuthenticated = !!user;
+    })
   }
 
   sendToDataStore() {
@@ -18,5 +26,9 @@ export class HeaderComponent implements OnInit {
 
   fetchFromStore() {
     this.dsService.fetchRecipe().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
